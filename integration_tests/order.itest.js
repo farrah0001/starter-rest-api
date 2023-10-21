8004a5be0a56f5309748f28ce0e5d451186486e5
Farrah Zakir <farrahzakir@gmail.com>
2023-10-21 14:12:12 +0200

    update cyclic app link

--- integration_tests/order.itest.js
+++ integration_tests/order.itest.js
@@ -2,7 +2,7 @@ const assert = require('assert')
 
 // check if the endpoint to get all orders brings orders before deploying it to production for customers
 async function testGetOrders () {
-  const response = await fetch('https://green-poncho.cyclic.app/order')
+  const response = await fetch('https://amused-parka-lamb.cyclic.app/order')
   const data = await response.json()
   // return data
   assert(response.status === 200, 'Response status code was: ' + response.status)
